6f096bda27b7903589322dce09c06c3347462aab
Takayuki Murooka <takayuki5168@gmail.com>
2018-08-19 10:37:52 +0900

    po

--- map.js
+++ map.js
@@ -1,6 +1,4 @@
 var map;
-var marker;
-var info_window;
 var center = {
     lat: 34.7,
     lng: 135.51
@@ -12,20 +10,29 @@ function initMap() {
 	zoom: 8
     });
 
-    marker = new google.maps.Marker({ // add marker
-	position: center,
+    map.addListener('click', function(e) { // add click event
+	if (true) { // mode = 1
+	    addMarker(e.latLng);
+	}
+    });
+
+    addMarker(center);
+}
+
+function addMarker(lat_lng) {
+    var marker = new google.maps.Marker({
+	position: lat_lng,
 	map: map
     });
 
-    info_window = new google.maps.InfoWindow({ // add info_window
+    var info_window = new google.maps.InfoWindow({ // add info_window
 	content: '<div class="map">test</div>'
     });
     marker.addListener('click', function() { // when marker is clicked
 	if (info_window.getMap() == null) {
 	    info_window.open(map, marker);
 	}else {
-	    info_window.close(map, marker);	    
+	    info_window.close(map, marker);
 	}
     });
-    
 }
